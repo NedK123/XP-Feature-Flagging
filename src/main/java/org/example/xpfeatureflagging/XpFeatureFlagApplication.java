6b4a8fabebba396ad595786b6b1d96a56b1bd65b
naderkahwaji <nader.kahwaji@gmail.com>
2024-06-05 21:54:24 +0300

    change to java

---
+++ src/main/java/org/example/xpfeatureflagging/XpFeatureFlagApplication.java
@@ -0,0 +1,13 @@
+package org.example.xpfeatureflagging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpFeatureFlagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpFeatureFlagApplication.class, args);
+    }
+
+}
